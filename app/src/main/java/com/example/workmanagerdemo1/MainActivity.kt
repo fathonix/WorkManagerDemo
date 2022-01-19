2fe6cba7d4f08488198a34d4c175d793b0730fd5
dzaky1201 <dzakyhdr@gmail.com>
2022-01-19 09:37:53 +0700

    create starter

--- app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
@@ -22,46 +22,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            setOneTimeWorkRequest()
+
         }
 
     }
 
     private fun setOneTimeWorkRequest() {
-        val workManager = WorkManager.getInstance(applicationContext)
 
-        // constraint digunakan untuk syarat worker akan berjalan apabila constraint terpenuhi
-        val constraints = Constraints.Builder()
-            // bisa berjalan jika hp di cas dan internet konek
-            .setRequiresCharging(true)
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-
-        // untuk membuat input ke dalam worker UploadWorker.kt
-        val data = Data.Builder()
-            .putInt(KEY_COUNT_VALUE, 125)
-            .build()
-
-        //membuat work request
-        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
-            .setConstraints(constraints)
-            .setInputData(data)
-            .build()
-
-        workManager.enqueue(uploadRequest)
-
-        // mendapatkan status workmanager
-        workManager.getWorkInfoByIdLiveData(uploadRequest.id)
-            .observe(this, Observer {
-                binding.status.text = it.state.name
-
-                // menerima data dari UploadWorker.kt, akan diterima ketika success
-                if (it.state.isFinished) {
-                    val data = it.outputData
-                    val message = data.getString(UploadWorker.KEY_WORKER)
-                    Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
-                }
-            })
     }
 }
 
--- app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
+++ app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
@@ -1,13 +1,10 @@
 package com.example.workmanagerdemo1
 
 import android.content.Context
-import android.util.Log
-import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import java.lang.Exception
-import java.text.SimpleDateFormat
-import java.util.*
+
 
 class UploadWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
@@ -18,20 +15,9 @@ class UploadWorker(context: Context, workerParams: WorkerParameters) :
 
     override fun doWork(): Result {
         return try {
-            val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE, 0)
-            for (a in 0 until count) {
-                Log.d("MYTAG", "uploading $a")
-            }
 
-            val time = SimpleDateFormat("dd/M/yyyy hh:mm:ss", Locale.getDefault())
-            val currentDate = time.format(Date())
 
-            // mengirim data ke mainActvity ini disebut dengan output data
-            val outputData = Data.Builder()
-                .putString(KEY_WORKER, currentDate)
-                .build()
-
-            Result.success(outputData)
+            Result.success()
         } catch (e: Exception) {
             Result.failure()
         }
